ee5e05304154c711bc6c5daee88590921f7a38b6
Bastian Buch <mail@bastian-buch.de>
2017-03-28 13:07:03 +0200

    Initial project commit

---
+++ src/main/java/de/bb/GrowthBricksDemoApplication.java
@@ -0,0 +1,12 @@
+package de.bb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthBricksDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthBricksDemoApplication.class, args);
+	}
+}
